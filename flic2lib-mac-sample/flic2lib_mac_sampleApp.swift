a389d4a29fb9d0e79026ec285fd4bb354cae8fcb
Oskar Öberg <oskar@oskob.me>
2025-12-10 09:46:29 +0100

    mac sample

---
+++ flic2lib-mac-sample/flic2lib_mac_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  flic2lib_mac_sampleApp.swift
+//  flic2lib-mac-sample
+//
+//  Created by Oskar Öberg on 2024-03-05.
+//
+
+import SwiftUI
+
+@main
+struct flic2lib_mac_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
